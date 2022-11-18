a6a368bf21dd29e0b535e319be1ccb6bf977f65f
Noor Fahad <noor@drillmaps.com>
2022-11-18 20:30:14 +0530

    Initial Commit

---
+++ launcher/launcherApp.swift
@@ -0,0 +1,17 @@
+//
+//  launcherApp.swift
+//  launcher
+//
+//  Created by Noor Fahad on 18/11/22.
+//
+
+import SwiftUI
+
+@main
+struct launcherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
